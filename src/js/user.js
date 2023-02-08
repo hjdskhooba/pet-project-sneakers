de499e5368c0a152317016108ce55c514064092b
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-02-08 18:36:22 +0300

    Скоро закончу регистрацию делать

---
+++ src/js/user.js
@@ -0,0 +1,12 @@
+
+let user = {
+    login: '',
+    email: ''
+}
+
+let formRegister = document.querySelector('.register-form')
+
+formRegister.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log('123')
+})
